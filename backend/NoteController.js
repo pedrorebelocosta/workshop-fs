49e92cebc79b4938399aabc1cd8efc4c177513fb
Pedro Costa <pedrorebelocosta@gmail.com>
2021-03-14 15:24:19 +0000

    BLL implemented, needs final revision

--- backend/NoteController.js
+++ backend/NoteController.js
@@ -2,15 +2,48 @@ const Note = require('./Note');
 
 module.exports = {
 	create: function (req, res, next) {
+		/* 
+			Para efeitos de praticalidade, vamos assumir que os dados estão a ser
+			recebidos com o formato correto
+		*/
+		const note = new Note(req.body);
+
+		Note.create(note, (err, doc) => {
+			if (err) next(err);
+			else res.status(201).end();
+		});
 
 	},
 	readAll: function (req, res, next) {
-
+		Note.find({}, (err, docs) => {
+			if (err) next(err);
+			else res.status(200).json(docs);
+		});
 	},
-	update: function (req, res, next) {
+	update: async function (req, res, next) {
+		const noteID = req.params.id;
+		const note = await Note.findOne({ _id: noteID });
+
+		if (!note) return;
+
+		const updatedNote = await Note.findOneAndUpdate(
+			{ _id: noteID }, req.body,
+			{ new: true }
+		);
+
+		if (!updatedNote) return res.status(500).end();
+		return res.status(200).json(updatedNote);
 
 	},
 	delete: function (req, res, next) {
+		const noteID = req.params.id;
+		const note = await Note.findOne({ _id: noteID });
 
+		if (!note) return;
+
+		Note.findOneAndRemove({ _id: noteID }, (err, doc) => {
+			if (err) next(err);
+			else return res.status(202).json(doc);
+		});
 	}
 }
